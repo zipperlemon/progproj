9bfb1b6a5f50617899021383054b40e8a63f81b0
zipperlemon <supremezipperlemon@gmail.com>
2019-10-29 21:30:32 +0100

    Update user_gui.py

--- user_gui.py
+++ user_gui.py
@@ -2,8 +2,9 @@ from tkinter import *
 import tkinter.messagebox, re
 from datetime import date
 
+
 def maxCharTweet(event):
-    if len(tweetContent.get("1.0", "end-1c")) == 140:
+    if len(tweetContent.get("1.0", "end-1c")) == 137:
         if event.keysym == "BackSpace":
             tweetContent.configure(state="normal")
         else:
@@ -13,7 +14,7 @@ def maxCharTweet(event):
 
 def maxCharName(event):
     print(len(nameInput.get()))
-    if len(nameInput.get()) == 139:
+    if len(nameInput.get()) == 136:
         if event.keysym == "BackSpace":
             nameInput.configure(state="normal")
         else:
@@ -21,7 +22,7 @@ def maxCharName(event):
 
 
 def clearName(event):
-   nameInput.delete(0, END)
+    nameInput.delete(0, END)
 
 
 def saveContent(content, name):
@@ -29,7 +30,7 @@ def saveContent(content, name):
     try:
         today = date.today().strftime("%d-%m-%y")
         tweets = open(path + today + ".txt", 'a')
-        tweets.write("{{{}}} <{}>".format(content, name))
+        tweets.write("{{{}}}.<{}>;".format(content, name))
     except Exception as e:
         print(e)
 
@@ -43,45 +44,41 @@ def checkContent():
     content = removeBadChars(tweetContent.get("1.0", "end-1c"))
     name = removeBadChars(nameInput.get())
 
-
-
-    if 0 < len(content) <= 140 and 0 < len(name) <= 139:
+    if 0 < len(content) <= 137 and 0 < len(name) <= 136 and len(content) + len(name) <= 137:
         if "naam" in name:
             tkinter.messagebox.showwarning(title="Er ging iets fout", message="Vul een geldige naam in!")
         else:
             saveContent(content, name)
     else:
-        tkinter.messagebox.showwarning(title="Er ging iets fout", message="De lengte van je tweet of je naam is niet correct!")
+        tkinter.messagebox.showwarning(title="Er ging iets fout",
+                                       message="De lengte van je tweet of je naam is niet correct!")
         return False
-
-
-
 
 
 HEIGHT = 550
 WIDTH = 700
 
 root = Tk();
-root.configure(bg ="#f7d417")
+root.configure(bg="#f7d417")
 
-canvas = Canvas(root, height=HEIGHT, width=WIDTH, bd=0, highlightthickness=0, relief='ridge', bg ="#f7d417")
+canvas = Canvas(root, height=HEIGHT, width=WIDTH, bd=0, highlightthickness=0, relief='ridge', bg="#f7d417")
 canvas.pack()
 
-title = Label(root, text="Tweet als NS", font="SegoeUI 22", fg="#003082", bg ="#f7d417")
+title = Label(root, text="Tweet als NS", font="SegoeUI 22", fg="#003082", bg="#f7d417")
 title.place(relx=0.50, rely=0, anchor='n')
 
-#tweetContent = Entry(root, font="Helvetica 22", justify='center')
-#tweetContent.place(relx=0.5, rely=0.1, relwidth=0.75, relheight=0.8, anchor='n')
+# tweetContent = Entry(root, font="Helvetica 22", justify='center')
+# tweetContent.place(relx=0.5, rely=0.1, relwidth=0.75, relheight=0.8, anchor='n')
 
 tweetContent = Text(root, font="SegoeUI 22", wrap='word')
 tweetContent.place(relx=0.5, rely=0.15, relwidth=0.75, relheight=0.5, anchor='n')
 tweetContent.bind("<KeyPress>", maxCharTweet)
 
-#lower_frame = Frame(root, bd=10)
-#lower_frame.place(relx=0.5, rely=0.75, relwidth=0.75, relheight=0.1, anchor='n')
+# lower_frame = Frame(root, bd=10)
+# lower_frame.place(relx=0.5, rely=0.75, relwidth=0.75, relheight=0.1, anchor='n')
 
 nameInput = Entry(root, font="Helvetica 22")
-#onclick delete
+# onclick delete
 nameInput.insert(0, "Vul je naam in")
 nameInput.place(relx=0.5, rely=0.715, relwidth=0.75, relheight=0.08, anchor='n')
 nameInput.bind("<Button-1>", clearName)
@@ -90,19 +87,19 @@ nameInput.bind("<KeyPress>", maxCharName)
 submitButton = Button(root, text="Tweet!", font="SegoeUI 18", bg="#0063d3", fg="#FFF", command=checkContent)
 submitButton.place(relx=0.5, rely=0.85, relwidth=0.75, relheight=0.1, anchor='n')
 
-#frame = Frame(root, bg="#80c1ff", bd=5)
-#frame.place(relx=0.5, rely=0.1, relwidth=0.75, relheight=0.1, anchor='n')
+# frame = Frame(root, bg="#80c1ff", bd=5)
+# frame.place(relx=0.5, rely=0.1, relwidth=0.75, relheight=0.1, anchor='n')
 
-#entry = Entry(frame, font=40)
-#entry.place(relwidth=0.65, relheight=1)
+# entry = Entry(frame, font=40)
+# entry.place(relwidth=0.65, relheight=1)
 
-#button = Button(frame, text="Test button", font=40)
-#button.place(relx=0.7, relwidth=0.3, relheight=1)
+# button = Button(frame, text="Test button", font=40)
+# button.place(relx=0.7, relwidth=0.3, relheight=1)
 
-#lower_frame = Frame(root, bg="#80c1ff", bd=10)
-#lower_frame.place(relx=0.5, rely=0.25, relwidth=0.75, relheight=0.6, anchor='n')
+# lower_frame = Frame(root, bg="#80c1ff", bd=10)
+# lower_frame.place(relx=0.5, rely=0.25, relwidth=0.75, relheight=0.6, anchor='n')
 
-#label = Label(lower_frame)
-#label.place(relwidth=1, relheight=1)
+# label = Label(lower_frame)
+# label.place(relwidth=1, relheight=1)
 
 root.mainloop()
