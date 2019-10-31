6306dfc30aa1cb2d277c78a7b1b5e2362c0f7e7d
zipperlemon <supremezipperlemon@gmail.com>
2019-10-31 16:28:53 +0100

    Update user_gui.py

--- user_gui.py
+++ user_gui.py
@@ -5,17 +5,19 @@ import user as us
 
 
 def maxCharTweet(event):
+    """Zorgt dat het maximaal aantal tekens in de tweet niet wordt overschreden"""
     if len(tweetContent.get("1.0", "end-1c")) == 137:
+        # Als het maximaal aantal tekens in bereikt mag je alleen nog maar backspce gebruiken
         if event.keysym == "BackSpace":
             tweetContent.configure(state="normal")
         else:
             tweetContent.configure(state="disabled")
-    print(len(str(tweetContent.get("1.0", "end-1c"))))
 
 
 def maxCharName(event):
-    print(len(nameInput.get()))
+    """Zorgt dat het maximaal aantal tekens in de naam niet wordt overschreden"""
     if len(nameInput.get()) == 136:
+        # Als het maximaal aantal tekens in bereikt mag je alleen nog maar backspce gebruiken
         if event.keysym == "BackSpace":
             nameInput.configure(state="normal")
         else:
@@ -23,20 +25,25 @@ def maxCharName(event):
 
 
 def clearName(event):
+    """Maakt de naam input leeg"""
     nameInput.delete(0, END)
 
 
 def saveContent(content, name):
+    """Slaat de tweet en de naam op"""
     path = "tweets/"
     try:
+        # Slaat de info op in een txt file met als naam de datum van vandaag
         today = date.today().strftime("%d-%m-%y")
         tweets = open(path + today + ".txt", 'a')
         tweets.write("{{{}}}.<{}>;".format(content, name))
+        tweets.close()
     except Exception as e:
         print(e)
 
 
 def checkContent():
+    """Checkt de tweet en de naam"""
     content = us.removeBadChars(tweetContent.get("1.0", "end-1c"))
     name = us.removeBadChars(nameInput.get())
 
@@ -62,42 +69,25 @@ root.configure(bg="#f7d417")
 canvas = Canvas(root, height=HEIGHT, width=WIDTH, bd=0, highlightthickness=0, relief='ridge', bg="#f7d417")
 canvas.pack()
 
+# Titel
 title = Label(root, text="Tweet als NS", font="SegoeUI 22", fg="#003082", bg="#f7d417")
 title.place(relx=0.50, rely=0, anchor='n')
 
-# tweetContent = Entry(root, font="Helvetica 22", justify='center')
-# tweetContent.place(relx=0.5, rely=0.1, relwidth=0.75, relheight=0.8, anchor='n')
-
+# De tweet zelf
 tweetContent = Text(root, font="SegoeUI 22", wrap='word')
 tweetContent.place(relx=0.5, rely=0.15, relwidth=0.75, relheight=0.5, anchor='n')
 tweetContent.bind("<KeyPress>", maxCharTweet)
 
-# lower_frame = Frame(root, bd=10)
-# lower_frame.place(relx=0.5, rely=0.75, relwidth=0.75, relheight=0.1, anchor='n')
-
+# De naam input
 nameInput = Entry(root, font="Helvetica 22")
-# onclick delete
+# Verwijder die inhoud als er op word gedrukt
 nameInput.insert(0, "Vul je naam in")
 nameInput.place(relx=0.5, rely=0.715, relwidth=0.75, relheight=0.08, anchor='n')
 nameInput.bind("<Button-1>", clearName)
 nameInput.bind("<KeyPress>", maxCharName)
 
+# Submit alles
 submitButton = Button(root, text="Tweet!", font="SegoeUI 18", bg="#0063d3", fg="#FFF", command=checkContent)
 submitButton.place(relx=0.5, rely=0.85, relwidth=0.75, relheight=0.1, anchor='n')
-
-# frame = Frame(root, bg="#80c1ff", bd=5)
-# frame.place(relx=0.5, rely=0.1, relwidth=0.75, relheight=0.1, anchor='n')
-
-# entry = Entry(frame, font=40)
-# entry.place(relwidth=0.65, relheight=1)
-
-# button = Button(frame, text="Test button", font=40)
-# button.place(relx=0.7, relwidth=0.3, relheight=1)
-
-# lower_frame = Frame(root, bg="#80c1ff", bd=10)
-# lower_frame.place(relx=0.5, rely=0.25, relwidth=0.75, relheight=0.6, anchor='n')
-
-# label = Label(lower_frame)
-# label.place(relwidth=1, relheight=1)
 
 root.mainloop()
